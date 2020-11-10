0b1d4d76771d1d8eb077f7082e8ba776e2db968c
suneel kanas <suneel.kanas@gmail.com>
2020-11-10 21:44:59 +0530

    added header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Header } from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello Suneel Kanas</h1>
+     <Header />
     </div>
   );
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+export class Header extends React.Component {
+    render () {
+        return (<div className="header">
+            <h1>Jazz Drumming</h1>
+        </div>)
+    }
+}
